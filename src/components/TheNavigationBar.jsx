6fc95d7e8e3d3a67d6ac1f2401ff01bba0180cff
Akhigbe-E <akhigbemmanuel@gmail.com>
2022-08-11 19:39:26 +0100

    Develop component and animation -Navigation bar

--- src/components/TheNavigationBar.jsx
+++ src/components/TheNavigationBar.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+import Dot from "./Dot";
 import NavCart from "./NavCart";
+import { useSpring, animated } from "react-spring";
 
 const TheNavigationBar = () => {
+  const navLinksAnimation = useSpring({
+    from: { opacity: 0.25, y: -70 },
+    to: { opacity: 1, y: 0 },
+    delay: 2200,
+    config: {
+      tension: 280,
+      friction: 80,
+    },
+  });
   return (
-    <nav className="t-container py-10">
+    <nav className="py-10">
       <div className="flex items-center justify-between">
         {/* Logo */}
-        <div className="w-full"></div>
-        <ul className="flex items-center justify-end space-x-14 text-d-gray font-medium text-lg">
+        <div className="w-full">
+          <Dot />
+        </div>
+        <animated.ul
+          style={navLinksAnimation}
+          className="flex items-center justify-end space-x-14 text-d-gray font-medium text-lg"
+        >
           <li className="cursor-pointer">Product</li>
           <li className="cursor-pointer">About</li>
           <li className="cursor-pointer">Blog</li>
@@ -15,7 +31,7 @@ const TheNavigationBar = () => {
           <li>
             <NavCart />
           </li>
-        </ul>
+        </animated.ul>
       </div>
     </nav>
   );
